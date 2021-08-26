579e24637027f772b16bc9e26bedffb0afe33b8f
foa2chr <Alex.Fouraker@boschrexroth-us.com>
2021-08-26 10:35:38 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+      #name="sdk-py-datalayer-provider",
+      name="sqlite-datalayer-provider",
+      version="1.0.01",
+      description="Simple SQLite database",
+      author="A. Fouraker",
+      packages=['app', 'sample.schema'],
+      # https://stackoverflow.com/questions/1612733/including-non-python-files-with-setup-py
+      package_data={'./': ['sampleSchema.bfbs']},
+      scripts=['main.py'],
+      license="Copyright (c) 2020-2021 Bosch Rexroth AG, Licensed under MIT License"
+ )
