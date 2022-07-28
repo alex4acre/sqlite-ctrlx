a5bb552d256d3c18b837affcb01f5448841c5c3c
Alex Fouraker <alex.fouraker@boschrexroth-us.com>
2022-07-28 21:25:15 +0000

    end of day, developing for core 20

---
+++ build-snap-arm64.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+snapcraft clean --destructive-mode
+snapcraft --destructive-mode --target-arch=arm64 --enable-experimental-target-arch
+snapcraft clean --destructive-mode
--- main.py
+++ main.py
@@ -23,45 +23,46 @@
 # SOFTWARE.
 
 import os
+import sys
 from sqlite3.dbapi2 import Connection
 import time
 import sqlite3
 from sqlite3 import Error
 
-import datalayer
-from datalayer.variant import Variant
+import ctrlxdatalayer
+from ctrlxdatalayer.variant import Variant
+
+from helper.ctrlx_datalayer_helper import get_provider
 
 from app.sql_provider_node import SQLiteNode
 
-value_address_str_1 = "SQLite/terminal-1"
-value_address_str_2 = "SQLite/terminal-2"
-value_address_str_3 = "SQLite/terminal-3"
-value_address_str_4 = "SQLite/terminal-4"
+value_address_str_1 = "terminal-1"
+value_address_str_2 = "terminal-2"
+value_address_str_3 = "terminal-3"
+value_address_str_4 = "terminal-4"
 
+# addresses of provided values
+address_base = "SQLite/"
+type_address_string = "types/datalayer/string"
 
 def main():
 
-    with datalayer.system.System("") as datalayer_system:
+     with ctrlxdatalayer.system.System("") as datalayer_system:
         datalayer_system.start(False)
 
-        # This is the connection string for TCP in the format: tcp://USER:PASSWORD@IP_ADDRESS:PORT
-        # Please check and change according your environment:
-        # - USER:       Enter your user name here - default is boschrexroth
-        # - PASSWORD:   Enter your password here - default is boschrexroth
-        # - IP_ADDRESS: 127.0.0.1   If you develop in WSL and you want to connect to a ctrlX CORE virtual with port forwarding
-        #               10.0.2.2    If you develop in a VM (Virtual Box, QEMU,...) and you want to connect to a ctrlX virtual with port forwarding
-        #               192.168.1.1 If you are using a ctrlX CORE or ctrlX CORE virtual with TAP adpater
+        # Try SSL port 8443
+        provider, connection_string = get_provider(datalayer_system)
+        if provider is None:
+            print("ERROR Connecting", connection_string, "failed.")
+            sys.exit(1)
 
-        #connectionProvider = "tcp://boschrexroth:boschrexroth@127.0.0.1:2070"
-        connectionProvider = "tcp://boschrexroth:boschrexroth@192.168.1.1:2070"
+        print("INFO Connecting", connection_string, "succeeded.")
 
-        if 'SNAP' in os.environ:
-            connectionProvider = "ipc://"
+        with provider:  # provider.close() is called automatically when leaving with block
 
-        print("Connecting", connectionProvider)
-        with datalayer_system.factory().create_provider(connectionProvider) as provider:
+            print("Starting provider")
             result = provider.start()
-            if result is not datalayer.variant.Result.OK:
+            if result is not ctrlxdatalayer.variant.Result.OK:
                 print("ERROR Starting Data Layer Provider failed with:", result)
                 return
 
@@ -70,8 +71,6 @@ def main():
             provider_node_str_3 = provide_string(provider, value_address_str_3)
             provider_node_str_4 = provide_string(provider, value_address_str_4)
 
-            print("Start provider")
-            provider.start()
             print("Running endless loop...")
             while provider.is_connected():
                 time.sleep(1.0)  # Seconds
@@ -105,12 +104,19 @@ def main():
 
         datalayer_system.stop(True)
 
-def provide_string(provider: datalayer.provider, name):
+def provide_string(provider: ctrlxdatalayer.provider, name):
     # Create and register simple string provider node
     print("Creating string  provider node")
     variantString = Variant()
     variantString.set_string("Enter SQL script here. Use ';' as the last character to suppress result")
-    provider_node_str = SQLiteNode(provider, name, variantString)
+    provider_node_str = SQLiteNode(
+        provider, 
+        type_address_string, 
+        address_base + name,
+        name, 
+        "-",
+        "SQL-Terminal",
+        variantString)
     provider_node_str.register_node()
     return provider_node_str
 
